d6081208cc831a5a8c9d5c4b751f378bed35a569
Abhilash A S <abhilashsajeev@gmail.com>
2022-11-09 19:48:28 +0530

    SSG for index page

--- components/UserCardTile.js
+++ components/UserCardTile.js
@@ -7,22 +7,22 @@ const UserCardTile = ({ item }) => {
     router.push(`/tasks/${id}`);
   };
   return (
-    <Col sm="6">
-      <Card body>
-        <CardTitle tag="h5">{item.name.toUpperCase()}</CardTitle>
-        <CardText>
-          <p>{item.designation}</p>
-        </CardText>
-        <Button
-          color="primary"
-          onClick={() => {
-            handleNavigate(item.uid);
-          }}
-        >
-          View Tasks
-        </Button>
-      </Card>
-    </Col>
+    <>
+      <Col sm="6">
+        <Card body>
+          <CardTitle tag="h5">{item.name.toUpperCase()}</CardTitle>
+          <CardText>{item.designation}</CardText>
+          <Button
+            color="primary"
+            onClick={() => {
+              handleNavigate(item.uid);
+            }}
+          >
+            View Tasks
+          </Button>
+        </Card>
+      </Col>
+    </>
   );
 };
 export default UserCardTile;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,37 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  generateEtags: false,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
