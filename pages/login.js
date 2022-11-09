9f62eee299bf41a4c3ca52bd78f130f28115a3db
Abhilash A S <abhilashsajeev@gmail.com>
2022-11-09 21:07:10 +0530

    Adding meta and title tags

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Router from "next/router";
+import Head from "next/head";
 
 import { useAuth } from "../context/AuthUserContext";
 
@@ -39,54 +40,59 @@ export default function Home() {
   };
 
   return (
-    <Container className="text-center" style={{ padding: "40px 0px" }}>
-      <Row>
-        <Col>
-          <h2>Login</h2>
-        </Col>
-      </Row>
-      <Row style={{ maxWidth: "400px", margin: "auto" }}>
-        <Col>
-          <Form onSubmit={onSubmit}>
-            {error && <Alert color="danger">{error}</Alert>}
-            <FormGroup row>
-              <Label for="loginEmail" sm={4}>
-                Email
-              </Label>
-              <Col sm={8}>
-                <Input
-                  type="email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                  name="email"
-                  id="loginEmail"
-                  placeholder="Email"
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="loginPassword" sm={4}>
-                Password
-              </Label>
-              <Col sm={8}>
-                <Input
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                  id="loginPassword"
-                  placeholder="Password"
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Col>
-                <Button>Login</Button>
-              </Col>
-            </FormGroup>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Container className="text-center" style={{ padding: "40px 0px" }}>
+        <Row>
+          <Col>
+            <h2>Login</h2>
+          </Col>
+        </Row>
+        <Row style={{ maxWidth: "400px", margin: "auto" }}>
+          <Col>
+            <Form onSubmit={onSubmit}>
+              {error && <Alert color="danger">{error}</Alert>}
+              <FormGroup row>
+                <Label for="loginEmail" sm={4}>
+                  Email
+                </Label>
+                <Col sm={8}>
+                  <Input
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    name="email"
+                    id="loginEmail"
+                    placeholder="Email"
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="loginPassword" sm={4}>
+                  Password
+                </Label>
+                <Col sm={8}>
+                  <Input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    id="loginPassword"
+                    placeholder="Password"
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col>
+                  <Button>Login</Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
